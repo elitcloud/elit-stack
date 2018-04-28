730346611ff2219348cfd7183539d663b979476d
Gary Lai <garylai1990@gmail.com>
2018-04-28 12:36:59 -0400

    :construction: (vpc) description for vpc

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -1,8 +1,23 @@
+/*
+  This vpc moudle inherit the source: https://github.com/terraform-aws-modules/terraform-aws-vpc
+
+  We only create public and private subnets in our stack. Not sure do we need to seperate subnest by services, eg. database_subnets.
+
+  The CIRD block of the VPC use 10.0.0.0/16 as default 
+  Public subnets use CIRD blocks: 10.0.0.0/24, 10.0.1.0/24, 10.0.2.0/24
+  Private subnets use  CIDR blocks: 10.0.100.0/24, 10.0.101.0/24, 10.0.102.0/24
+
+  NAT gateway is enable for private subnets connection. 
+
+  # TODO: 
+    - azs should be flexible 
+    - public_subnest and private_subnets settings should be flexible
+ */
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "${var.name}"
-  cidr = "10.0.0.0/16"
+  name = "${var.name}-vpc"
+  cidr = "${var.cidr}"
 
   azs             = ["${format("%sa", var.region)}", "${format("%sb", var.region)}", "${format("%sc", var.region)}"]
   public_subnets  = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
--- terraform/vpc/variables.tf
+++ terraform/vpc/variables.tf
@@ -1,3 +1,7 @@
 variable name {}
 variable environment {}
 variable region {}
+
+variable cidr {
+  default = "10.0.0.0/16"
+}
