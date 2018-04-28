a0fef642ef479e1a9a05de4ef2a4a57e24c8baa7
Gary Lai <garylai1990@gmail.com>
2018-04-28 10:54:05 -0400

    :construction: (vpc) start from 0

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -5,8 +5,8 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["${format("%sa", var.region)}", "${format("%sb", var.region)}", "${format("%sc", var.region)}"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.100.0/24", "10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
