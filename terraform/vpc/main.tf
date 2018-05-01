70fde18abefd200b2360c98a57f45539941af563
Gary Lai <garylai1990@gmail.com>
2018-04-30 21:13:34 -0400

    :construction: (vpc) update vpc

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -11,23 +11,124 @@
 
   # TODO: 
     - azs should be flexible 
-    - public_subnest and private_subnets settings should be flexible
- */
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+    - public_subnets and private_subnets settings should be flexible
+  */
 
-  name = "${var.name}-vpc"
-  cidr = "${var.cidr}"
+# VPC
 
-  azs             = ["${format("%sa", var.region)}", "${format("%sb", var.region)}", "${format("%sc", var.region)}"]
-  public_subnets  = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.100.0/24", "10.0.101.0/24", "10.0.102.0/24"]
+resource "aws_vpc" "main" {
+  cidr_block           = "${var.cidr}"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
-
-  tags = {
-    Terraform   = "true"
+  tags {
+    Name        = "${var.name}"
     Environment = "${var.environment}"
   }
+}
+
+# Internet Gateway
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags {
+    Name        = "${var.name}"
+    Environment = "${var.environment}"
+  }
+}
+
+# NAT Gateway
+
+resource "aws_nat_gateway" "main" {
+  count         = "${length(var.private_subnets)}"
+  allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
+  subnet_id     = "${element(aws_subnet.public_subnets.*.id, count.index)}"
+  depends_on    = ["aws_internet_gateway.main"]
+}
+
+resource "aws_eip" "nat" {
+  count = "${length(var.private_subnets)}"
+  vpc   = true
+}
+
+# Subnets
+
+# Public subnet
+
+resource "aws_subnet" "public_subnets" {
+  count                   = "${length(var.public_subnets)}"
+  vpc_id                  = "${aws_vpc.main.id}"
+  cidr_block              = "${element(var.public_subnets, count.index)}"
+  availability_zone       = "${element(var.availability_zone, count.index)}"
+  map_public_ip_on_launch = true
+
+  tags {
+    Name        = "${var.name}-${format("public-%03d", count.index+1)}"
+    Environment = "${var.environment}"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count             = "${length(var.private_subnets)}"
+  vpc_id            = "${aws_vpc.main.id}"
+  cidr_block        = "${element(var.private_subnets, count.index)}"
+  availability_zone = "${element(var.availability_zone, count.index)}"
+
+  tags {
+    Name        = "${var.name}-${format("private-%03d", count.index+1)}"
+    Environment = "${var.environment}"
+  }
+}
+
+# Routing
+
+# Route table
+
+resource "aws_route" "internet_gateway" {
+  route_table_id         = "${aws_route_table.internet_gateway.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.main.id}"
+}
+
+resource "aws_route_table" "internet_gateway" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags {
+    Name        = "${var.name}-interet-gateway"
+    Environment = "${var.environment}"
+  }
+}
+
+resource "aws_route" "nat_gateway" {
+  count                  = "${length(var.private_subnets)}"
+  route_table_id         = "${element(aws_route_table.nat_gateway.*.id, count.index)}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${element(aws_nat_gateway.main.*.id, count.index)}"
+}
+
+resource "aws_route_table" "nat_gateway" {
+  count  = "${length(var.private_subnets)}"
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags {
+    Name        = "${var.name}-${format("nat-gateway-%03d", count.index+1)}"
+    Environment = "${var.environment}"
+  }
+}
+
+# Route table association 
+
+resource "aws_route_table_association" "public" {
+  count = "${length(var.public_subnets)}"
+
+  subnet_id      = "${element(aws_subnet.public_subnets.*.id, count.index)}"
+  route_table_id = "${aws_route_table.internet_gateway.id}"
+}
+
+resource "aws_route_table_association" "private" {
+  count = "${length(var.private_subnets)}"
+
+  subnet_id      = "${element(aws_subnet.private_subnets.*.id, count.index)}"
+  route_table_id = "${element(aws_route_table.nat_gateway.*.id, count.index)}"
 }
--- terraform/vpc/variables.tf
+++ terraform/vpc/variables.tf
@@ -5,3 +5,18 @@ variable region {}
 variable cidr {
   default = "10.0.0.0/16"
 }
+
+variable public_subnets {
+  type    = "list"
+  default = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable private_subnets {
+  type    = "list"
+  default = ["10.0.100.0/24", "10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable availability_zone {
+  type    = "list"
+  default = ["us-west-2a", "us-west-2b", "us-west-2c"]
+}
