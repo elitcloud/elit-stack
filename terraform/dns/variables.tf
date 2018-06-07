aaf225a2317389d472f055d503e400fcccb2a8e1
Gary Lai <garylai1990@gmail.com>
2018-06-07 13:51:59 -0400

    :construction: (dns)

--- terraform/dns/variables.tf
+++ terraform/dns/variables.tf
@@ -6,3 +6,9 @@ variable "vpc_id" {
   description = "The VPC ID (omit to create a public zone)"
   default     = ""
 }
+
+variable "domain" {}
+
+variable "lb_dns_name" {}
+
+variable "lb_zone_id" {}
